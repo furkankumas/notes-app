e883fc178c643367981e8b01f320f63a23a59cad
furkankumas <kumasfurkan971@gmail.com>
2023-10-30 09:56:42 +0300

    Added docker

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const { Sequelize } = require('sequelize')
 
-const initializeDB = new Sequelize(process.env.POSTGRES_DB, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {dialect: 'postgresql', logging: false})
+const initializeDB = new Sequelize(process.env.POSTGRES_DB, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {host: process.env.POSTGRES_HOST, port: '5432', dialect: 'postgresql', logging: false})
 initializeDB.sync()
 
 
